d62daf6e311e664d2a6ed32b7c164aa88a91b3c3
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-04-12 11:55:59 -0500

    Edited the Temp example

--- controls/First_and_Second_Order_Project/Temp_example.py
+++ controls/First_and_Second_Order_Project/Temp_example.py
@@ -6,25 +6,29 @@ import scipy.integrate as S
 ###or first order like this
 def Derivatives(T,t):
     ###The final temperature value
-    Tc = 42
-    ###Guess and check on this value
-    ###until your simulation matches your data
-    a = 0.1
+    Tc = 0
+    ###Time constant
+    tau = 9. ##seconds ##how long it takes to get to 68% of the final value
+    a = 1/tau ##heat capacitance variable
     ##This is a model of your system.
+    ## Tdot + a*T = a*Tc
     Tdot = -a*T + a*Tc
     
     return Tdot
 
 ###Generate some example data - This requires an experiment
 tdata = np.asarray([0,5,10,15,20,25,30])
-Tdata = np.asarray([70,60,52,45,42,42,42])
+Tdata = np.asarray([70,60,52,45,42,42,42])-42
 
 ##Plot the data
 plt.plot(tdata,Tdata,'b*')
+plt.grid()
+plt.xlabel('Time (sec)')
+plt.ylabel('Temperature (C)')
 
 ##Simulate the model you created above
 tout = np.linspace(0,30,1000)
-Tout = S.odeint(Derivatives,70,tout)
+Tout = S.odeint(Derivatives,27.8,tout)
 
 ###plot the simulation data
 plt.plot(tout,Tout,'r-')
