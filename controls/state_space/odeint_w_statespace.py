6f777dd5018e6a41acb5abe7d581072544956558
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-04-23 11:32:06 -0500

    Added a code on 2d state space

--- controls/state_space/odeint_w_statespace.py
+++ controls/state_space/odeint_w_statespace.py
@@ -3,18 +3,26 @@ import matplotlib.pyplot as plt
 import scipy.integrate as sint
 
 ##Create A and B Matrices
-A =
-B =
+c = 2.
+k = 10.
+m = 0.5
+A = np.asarray([[0,1],[-k/m,-c/m]])
+B = np.asarray([0,1/m])
+k1 = 100.0
+k2 = 5.0
+K = np.asarray([k1,k2])
+xc = np.asarray([10,0])
 
 ##create the derivatives routine
 def Derivatives(x,t):
-    
+    u = np.matmul(K,xc-x) ### u (1x1) = K (1x2) * x-xc (2x1)
+    #xdot = A*x + B*u
+    xdot = np.matmul(A,x) + B*u
     return xdot
-
 
 ##Create time vector and initial conditions
 tout = np.linspace(0,10,1000)
-xinitial =
+xinitial = np.asarray([0,-10])
 #Run the integrator
 xout = sint.odeint(Derivatives,xinitial,tout)
 
