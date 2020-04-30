d8deaf9c6c04f5542ce7f2d2ee42aab099066e25
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-04-30 16:46:53 -0500

    Added audio file and stuff

--- sound/import_audio_file.py
+++ sound/import_audio_file.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import array
 
 def FFTReimmannSIN(w,x):
     global dt
@@ -19,18 +20,38 @@ def FFTReimmannCOS(w,x):
         out += x[i]*(dt)*np.cos(w*ti)
     return out
 
-#####FOR FAKE DATA###########
-dt = 0.01
-N = 100
-nmax = N
-#Make fake accel data
-factual = 30.0
-t = np.arange(0,dt*N,dt)
-f = 9.81*np.sin(2*np.pi*factual*t)
-L = dt*N
+#####FOR SIM CPX DATA###########
+# dt = 1/16000.
+# N = 3200
+# nmax = 80
+# # #Make fake accel data
+# factual = 330.
+# t = np.arange(0,dt*N,dt)
+# f = 9.81*np.sin(2*np.pi*factual*t)
+# L = dt*N
+# print(L)
 ############################
 
-####USING AUDIO DATA USE THIS
+####ACTUAL CPX DATA
+## 16 cyles in our data snippet for 82 Hz signal
+## 16/82 = 0.1951
+## L = 0.1951
+## N = 1000
+## dt = L/1000 = 0.0001952
+## 1/0.0001952 = 5125 <- Is the sample rate???
+## 1/16000 =     0.0000625
+#dt = 1/5125.
+dt = 1/16000.
+N = 1000.
+nmax = 80
+t = np.arange(0,dt*N,dt)
+#f = np.loadtxt('Low_E.txt',delimiter=',')
+f = np.loadtxt('High_E.txt',delimiter=',')
+L = dt*N
+
+plt.plot(t[0:250],f[0:250]-32768-700)
+
+####USING AUDIO DATA
 #data = np.loadtxt('Audio_transposed.txt')
 # data = np.loadtxt('Audio_truncated.txt')
 # t = data[:,0]
@@ -51,6 +72,7 @@ bnMAX = 0.0
 bf_fund = 0
 af_fund = 0
 an_vec = [0]*nmax
+bn_vec = [0]*nmax
 for i in range(1,nmax):
     print('FFT frequency = ' + str(i) + ' out of ' + str(nmax))
     #Frequency
@@ -60,11 +82,12 @@ for i in range(1,nmax):
     ani = (2.0/L)*FFTReimmannSIN(w,f)
     bni = (2.0/L)*FFTReimmannCOS(w,f)
     an_vec[i] = abs(ani)
-    if abs(ani) > anMAX:
+    bn_vec[i] = abs(bni)
+    if abs(ani) >= anMAX:
         anMAX = abs(ani)
         awn_fund = w
         af_fund = (i-1)/L
-    if abs(bni) > bnMAX:
+    if abs(bni) >= bnMAX:
         bnMAX = abs(bni)
         bwn_fund = w
         bf_fund = (i-1)/L
@@ -73,7 +96,12 @@ if af_fund < bf_fund:
     f_fund = af_fund
 else:
     f_fund = bf_fund
-# plt.plot(an_vec)
-# plt.show()
+
+iters = np.arange(0,nmax)
+freqs = (iters-1)/L
+plt.figure()
+plt.plot(freqs,an_vec)
+plt.plot(freqs,bn_vec)
+plt.show()
 # print('Fundamental Frequency (Hz) = ',af_fund,bf_fund)
 print('Fundamental Frequency (Hz) = ',f_fund)
---
+++ sound/low_e_note.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#t = np.linspace(0,0.0602,10000)
+#y = np.sin(2*np.pi*84*t)
+
+#plt.plot(t,y)
+#plt.show()
+
+L = 0.0602
+N = 1000
+dt = L/N
+print(dt)
