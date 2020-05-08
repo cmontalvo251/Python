f857f087e44e49eb670e2e82bbf0a021b7b7ec21
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-05-08 12:08:59 -0500

    Fixed the general kalman filter code

--- nonlinear_controls/kalman_1st_order_general.py
+++ nonlinear_controls/kalman_1st_order_general.py
@@ -1,62 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-S = 0.6
-rho = 1.225
-SensorPeriod = 10
+SensorPeriod = 1.0
 lastSensorTime = -2*SensorPeriod
+ftilde = -3.2
 
 def sensor(x,t):
     global lastSensorTime
     if (t-lastSensorTime) > SensorPeriod:
         lastSensorTime = t
-        y = x + 0.1*np.sin(100*t)
+        v = 0.01*np.sin(100*t)
+        y = x + v
         return y
     else:
         return -99
     
-def control(state,t):
+def control(xm,t):
     ##Control law - make sure to use the model estimate and not the actual truth signal
-    xm = state[1]
     xc = 1.0
     e = xc - xm
-    u = 0.
+    kp = 0.2
+    u = kp*e
     return u
 
-def Physics_Engine(t,state,u):
-    x = state[0]
-    xm = state[1]
-    p = state[2]
-    
-    ##Code in your model dynamics
-    xmdot = 0.
-        
+def Covariance_Engine(t,p):
+    global ftilde
+    ##Code in your Covariance Dynamics
+    # q = E[ww^T]
+    q = 10.0
+    pdot = ftilde*p + p*ftilde + q
+    return pdot
+
+def Physics_Engine(t,x,u):
     #Disturbance
-    w = 0.
+    w = 0*np.sin(3*t)
     
     ##Code in your state dynamics
-    xdot = 0.
+    xdot = -3*x + 4*u + w
+
+    return xdot
+
+def Model_Engine(t,xm,u):
+    global ftilde
+    ##Code in your model dynamics
+    xmdot = ftilde*xm + 3.9*u
     
-    ##Code in your Covariance Dynamics
-    pdot = 0.
+    return xmdot
     
-    statedot = np.asarray([xdot,xmdot,pdot])
-    return statedot
 
 ##Setup time stuff
 tinitial = 0
 tfinal = 100
-timestep = 0.1
+timestep = 0.01
 
 #Then we can run the RK4 engine
 t = tinitial
 print('Begin RK4 Integrator')
-x0 = 0.
-xm0 = 0.
-p0 = 0.
-state = np.asarray([x0,xm0,p0])
+x = 0.
+xm = 0.
+p = 0.
 tout = np.arange(tinitial,tfinal,timestep)
-stateout = np.zeros((len(tout),len(state)))
+xout = np.zeros((len(tout),1))
+xmout = 0*xout
+pout = 0*xout
 uout = 0*tout
 ctr = 0
 tmeasure = []
@@ -64,50 +70,80 @@ ybarout = []
 for ctr in range(0,len(tout)):
     
     ##Call the controller
-    u = control(state,t)
+    u = control(xm,t)
     
     #Save states for plotting
-    stateout[ctr,:] = state
+    xout[ctr] = x
+    xmout[ctr] = xm
+    pout[ctr] = p
     uout[ctr] = u
     
     ##Extract time
     t = tout[ctr]
     
     ##See if we have a new measurement?
-    y = sensor(state[0],t)
-    if (y != -99):
-        ybar = y
+    ybar = sensor(x,t)
+    if (ybar != -99):
         ######Compute K
-        K = 0.
+        #r = E[vv^T]
+        r = 0.01
+        K = p/(p+r)
         #Get a new model state
-        state[0] = state[0] + K*(ybar-state[1])
+        xm = xm + K*(ybar-xm)
         #Get a new covariance
-        state[2] = (1-K)*state[2]
+        p = (1-K)*p
         #Save the measurement
         ybarout.append(ybar)
         tmeasure.append(t)
         
     ##Integrate the Model Use RK4
-    k1 = Physics_Engine(t,state,u)
-    k2 = Physics_Engine(t+timestep/2.0,state+k1*timestep/2.0,u)
-    k3 = Physics_Engine(t+timestep/2.0,state+k2*timestep/2.0,u)
-    k4 = Physics_Engine(t+timestep,state+k3*timestep,u)
+    k1 = Physics_Engine(t,x,u)
+    k2 = Physics_Engine(t+timestep/2.0,x+k1*timestep/2.0,u)
+    k3 = Physics_Engine(t+timestep/2.0,x+k2*timestep/2.0,u)
+    k4 = Physics_Engine(t+timestep,x+k3*timestep,u)
     phi = (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4)
     #Step State
-    state += phi*timestep
+    x += phi*timestep
+    
+    ##Integrate the Model Use RK4
+    k1 = Model_Engine(t,xm,u)
+    k2 = Model_Engine(t+timestep/2.0,xm+k1*timestep/2.0,u)
+    k3 = Model_Engine(t+timestep/2.0,xm+k2*timestep/2.0,u)
+    k4 = Model_Engine(t+timestep,xm+k3*timestep,u)
+    phi = (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4)
+    #Step State
+    xm += phi*timestep
+    
+    ##Integrate the Model Use RK4
+    k1 = Covariance_Engine(t,p)
+    k2 = Covariance_Engine(t+timestep/2.0,p+k1*timestep/2.0)
+    k3 = Covariance_Engine(t+timestep/2.0,p+k2*timestep/2.0)
+    k4 = Covariance_Engine(t+timestep,p+k3*timestep)
+    phi = (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4)
+    #Step State
+    p += phi*timestep
+    
     print('Time =',t)
     
 print('RK4 Integration Complete')
 
+plt.close("all")
+
 #plot Everything
 plt.figure()
-plt.plot(tout,stateout[:,0],label='State')
-plt.plot(tout,stateout[:,1],label='Model')
+plt.plot(tout,xout,label='State')
+plt.plot(tout,xmout,label='Model')
 plt.plot(tmeasure,ybarout,'b*',label='Sensor Measurement')
 plt.grid()
 plt.xlabel('Time (sec)')
 plt.ylabel('State')
 plt.legend()
+
+plt.figure()
+plt.plot(tout,pout)
+plt.grid()
+plt.xlabel('Time (sec)')
+plt.ylabel('Covariance')
 
 plt.figure()
 plt.plot(tout,uout)
