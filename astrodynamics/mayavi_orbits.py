3888b0584b7a1e264dce652b13d8351b99dad48e
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-10-02 16:18:13 -0500

    Added some mayavi examples

--- astrodynamics/mayavi_orbits.py
+++ astrodynamics/mayavi_orbits.py
@@ -1,7 +1,8 @@
 import numpy as np
+from mayavi import mlab
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
+###Alright Let's plot an orbit around Kerbin using this new Mayavi Library
 ##Kerbin Parameters
 G = 6.6742*10**-11; #%%Gravitational constant
 Mkerbin = 5.2915158*10**22 #
@@ -24,7 +25,7 @@ a = (ra+rp)/2.
 ##Eccentricity
 e = (ra - rp)/(ra+rp)
 ##inclination
-i = 0*98.0*np.pi/180.0 ##Drew's random satellite he wants a just slightly over polar retrograde orbit
+i = 45.0*np.pi/180.0 ##Drew's random satellite he wants a just slightly over polar retrograde orbit
 ###Longitude of the Ascending Node
 W = 0*45*np.pi/180.0
 #Argument of the periaps
@@ -65,21 +66,9 @@ for x in range(0,len(xp)):
   xi[x] = xyzi[0]
   yj[x] = xyzi[1]
   zk[x] = xyzi[2]
-  
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-xsph = np.cos(u)*np.sin(v)
-ysph = np.sin(u)*np.sin(v)
-zsph = np.cos(v)
-ax.plot_surface(Rkerbin*xsph,Rkerbin*ysph,Rkerbin*zsph,color='blue',zorder=1)
-ax.plot(xi,yj,zk,'r-',zorder=0)
-ax.scatter(xi[0],yj[0],zk[0],'r*',s=20)
-#ax.plot_wireframe(RMun*xsph,RMun*ysph,RMun*zsph,color='grey')
-#ax.axis('square')
 
-###Now let's plot velocity
+###Let's plot velocity
 vx = np.sqrt(muKerbin/p)*(-np.sin(nu))
 vy = np.sqrt(muKerbin/p)*(e+np.cos(nu))
 #vx = np.sqrt(muMun/p)*(-np.sin(nu))
@@ -94,6 +83,45 @@ plt.grid()
 plt.xlabel('True Anomaly (rad)')
 plt.ylabel('Velocity (m/s)')
 plt.legend()
-    
-plt.show()
 
+###From here we can plot the orbit in 3D using matplotlib.
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+xsph = np.cos(u)*np.sin(v)
+ysph = np.sin(u)*np.sin(v)
+zsph = np.cos(v)
+ax.plot_surface(Rkerbin*xsph,Rkerbin*ysph,Rkerbin*zsph,color='blue',zorder=1)
+ax.plot(xi,yj,zk,'r-',zorder=0)
+ax.scatter(xi[0],yj[0],zk[0],'r*',s=20)
+#ax.plot_wireframe(RMun*xsph,RMun*ysph,RMun*zsph,color='grey')
+#ax.axis('square')
+
+#plt.show()
+
+##Unfortunately When you use matplotlib the orbit does not plot properly.
+##So instead we will use this mayavi module
+##First let's plot the orbit of the satellite
+#dphi, dtheta = np.pi/250.0, np.pi/250.0
+#theta0 = np.arange(0, 2*np.pi+dtheta*1.5, dtheta) #
+#orbit_radius = 5
+#xorb = orbit_radius * np.cos(theta0)
+#yorb = orbit_radius * np.sin(theta0)
+#zorb = 0 * np.cos(theta0)
+#s = mlab.plot3d(xorb,yorb,zorb)
+f = 100000
+s=mlab.plot3d(xi/f,yj/f,zk/f)
+
+##Then let's plot a sphere the size of the satellite
+rsat = 0.1*Rkerbin # km?
+s=mlab.mesh((rsat*xsph+xi[0])/f,(rsat*ysph+yj[0])/f,(rsat*zsph+zk[0])/f,color=(0.7,0.7,0.7))
+
+##Finally let's plot kerbin in blue at the center of the orbit
+s=mlab.mesh(Rkerbin*xsph/f,Rkerbin*ysph/f,Rkerbin*zsph/f,color=(0.0,0.9,0.0))
+
+##Then we set the proper view port
+#mlab.view(azimuth=-0, elevation=45, distance=Rkerbin*10)
+mlab.view(azimuth=-0, elevation=45, distance=5*ra/f)
+mlab.orientation_axes()
+#mlab.savefig(f'moonearth/Above{int(time*10):04d}.png')
+mlab.show()
