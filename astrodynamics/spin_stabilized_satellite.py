10664012d624863731c4a0ebaef0a5e082fdc2df
Carlos Montalvo <cmontalvo@southalabama.edu>
2020-10-24 07:03:31 -0500

    Finished the nutation angle stabilization

--- astrodynamics/spin_stabilized_satellite.py
+++ astrodynamics/spin_stabilized_satellite.py
@@ -9,8 +9,11 @@ pulse_on_time = 0.0
 Number_of_Pulses = 0.0
 flip_over = 0
 spin_backup = 0
+nutation_wait = 0
+roll_min = 0
+tlast_min = 0.
 def Thrusters(state,t):
-    global thrusters,pulse_off_time,pulse_on_time,pulse_wait_duration,pulse_duration,Number_of_Pulses,flip_over,spin_backup
+    global thrusters,pulse_off_time,pulse_on_time,pulse_wait_duration,pulse_duration,Number_of_Pulses,nutation_wait,roll_min,tlast_min
     ###Thrusters
     
     ###Torque on the spacecraft
@@ -21,42 +24,47 @@ def Thrusters(state,t):
         ##Do we need to turn them off?
         if (t >= pulse_on_time + pulse_duration):
             thrusters = 0
+            nutation_wait = 1
             pulse_off_time = t
     else:
         #Thrusters are currently off
         #Can I turn them back on?
-        if (t >= pulse_off_time + pulse_wait_duration):
+        if (t >= pulse_off_time + pulse_wait_duration) and nutation_wait == 0:
             thrusters = 1.0
             pulse_on_time = t
     
     r = 1.0
     F = 4.0*50.0*thrusters
+    wx = state[0]
+    wy = state[1]
     wz = state[2]
+    w = state[0:3]
+    what = w/np.linalg.norm(w)
+    zaxis = np.asarray([0,0,1])
+    sinangle_n = np.cross(what,zaxis)
+    sinangle = np.linalg.norm(sinangle_n)
+    precession_angle = np.arcsin(sinangle)
+    #print(precession_angle)
+    pitch = state[4]
     roll = state[3]
-    Torque = np.asarray([0,0,0])
-    if flip_over == 0:
-        if wz > 0:
-            Torque = np.asarray([0,0,-r*F])
-        else:
-            if thrusters == 1:
-                print('Flipping Over')
-                Torque = np.asarray([r*F,0,0])
-                print(Torque)
-                flip_over = 1
-    else:
-        if spin_backup == 1:
-            Torque = np.asarray([0,0,r*F])
-            if wz > 2000.0/500.0:
-                Torque = np.asarray([0,0,0])
-        if roll > np.pi and spin_backup == 0 and thrusters == 1:
-            Torque = np.asarray([-r*F,0,0])
-            spin_backup = 1
-            print('Time to spin back up',t)
-            
+    
+    precession_angle = np.sqrt(roll**2 + pitch**2)
+    
+    if roll < roll_min:
+        roll_min = roll
+        delt = t - tlast_min
+        tlast_min = t
+        #print(roll_min,t,delt)
+        if delt > 1.0:
+            nutation_wait = 0
+    
+    #precession_angle = np.sqrt(roll**2 + pitch**2)
+    Torque = np.asarray([0,r*F,0])
+    
     if np.sum(Torque) != 0 and pulse_on_time == t:
         Number_of_Pulses+=1
         
-    return Torque
+    return Torque,precession_angle
 
 def Derivatives(state,t,Torque):
     rpy = state[3:6]
@@ -88,22 +96,24 @@ def Derivatives(state,t,Torque):
     return np.hstack([dwdt,drpydt])
     
 ####So now we integrate in odeint
-dt = 10.0/1000.0
-tout = np.arange(0,80,dt)
+dt = 1.0/1000.0
+tout = np.arange(0,1000.0,dt)
 stateout = np.zeros((6,len(tout)))
 
 stateout[:,0] = np.asarray([0,0,2000./500.,0,0,0])
 pulses = 0*tout
+precessionout = 0*tout
 
 for ctr in range(0,len(tout)-1):
     ti = tout[ctr]
     statei = stateout[:,ctr]
     ###RK4
-    Torque = Thrusters(statei,ti)
+    Torque,precession_angle = Thrusters(statei,ti)
     if np.sum(Torque) == 0:
         pulses[ctr] = 0
     else:
-        pulses[ctr] = thrusters    
+        pulses[ctr] = thrusters
+    precessionout[ctr] = precession_angle
     dstate1 = Derivatives(statei,ti,Torque)
     dstate2 = Derivatives(statei+dstate1*dt/2.0,ti+dt/2.0,Torque)
     dstate3 = Derivatives(statei+dstate2*dt/2.0,ti+dt/2.0,Torque)
@@ -112,7 +122,7 @@ for ctr in range(0,len(tout)-1):
     stateout[:,ctr+1] = statei + dt*chi
 
 print('Number of Pulses = ',Number_of_Pulses)
-
+#print(precessionout)
 wout = stateout[0:3,:]
 rp = stateout[3:5,:]
 
@@ -133,5 +143,11 @@ plt.plot(tout,pulses)
 plt.grid()
 plt.xlabel('Time (sec)')
 plt.ylabel('Pulses')
+
+plt.figure()
+plt.plot(tout,precessionout)
+plt.grid()
+plt.xlabel('Time (sec)')
+plt.ylabel('Precession Angle (rad)')
 
 plt.show()
