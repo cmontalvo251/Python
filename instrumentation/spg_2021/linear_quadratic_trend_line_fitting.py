7588c6e1af7245d43602de7a050ecfb2808e0fb5
Carlos Montalvo T540p <cmontalvo251@gmail.com>
2021-03-29 13:59:25 -0500

    Changed some shit

--- instrumentation/spg_2021/linear_quadratic_trend_line_fitting.py
+++ instrumentation/spg_2021/linear_quadratic_trend_line_fitting.py
@@ -7,21 +7,26 @@ def speed_linear(voltage):
 def speed_quadratic(voltage):
     return -0.0675*voltage**2 + 3.833*voltage - 34.29
 
+def speed_cubic(v):
+    return -1.21e-2*v**3 + 0.567*v**2 - 6.97*v + 25.3
+
 volt = np.asarray([12,13,14,15,16,17,18,19,23])  ##voltage applied to motor
 speed = np.asarray([3.0,3.5,5.0,8.0,10.0,11.5,13.5,14.0,18.0]) #speed in m/s
 
-coeff = np.polyfit(volt,speed,2)
+coeff = np.polyfit(volt,speed,3)
 
 volt_fit = np.linspace(np.min(volt),np.max(volt),100)
 speed_fit = np.polyval(coeff,volt_fit)
 
 speed_manual_linear = speed_linear(volt_fit)
 speed_manual_quadratic = speed_quadratic(volt_fit)
+speed_manual_cubic = speed_cubic(volt_fit)
 
 plt.plot(volt,speed,'b*',label='Experimental Data')
 plt.plot(volt_fit,speed_fit,'r-',label='Trend Line Fit')
 plt.plot(volt_fit,speed_manual_linear,'k-',label='Manually Entering Trend Line (Linear)')
 plt.plot(volt_fit,speed_manual_quadratic,'y-',label='Manually Entering Trend Line (Quadratic)')
+plt.plot(volt_fit,speed_manual_cubic,'g-',label='Manual Trend Line (Cubic)')
 plt.grid()
 plt.legend()
 plt.title('Coeff = '+str(coeff))
