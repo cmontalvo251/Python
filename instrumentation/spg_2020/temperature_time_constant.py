01fe1e7cdfb1153bb821bda033993826245c2056
Carlos Montalvo <cmontalvo@southalabama.edu>
2021-04-12 15:36:26 -0500

    Added a reimann example

--- instrumentation/spg_2020/temperature_time_constant.py
+++ instrumentation/spg_2020/temperature_time_constant.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 cool = np.loadtxt('Cool_Down.txt')
-heat = np.loadtxt('Heat_Up.txt')
+#heat = np.loadtxt('Heat_Up.txt')
 
-time_cool = cool[:,0]
+time0 = 76.0 ##This is how much I need to shift the plot to the left to have my graph start at t=0
+time_cool = cool[:,0]-time0
 temp_cool = cool[:,1]
 plt.figure()
 plt.plot(time_cool,temp_cool,'b*',label='Measured Data')
@@ -12,35 +13,16 @@ plt.grid()
 plt.xlabel('Time (sec)')
 plt.ylabel('Temperature (C)')
 
-###Time Constant
-t0 = 77.831
-TsC = 720-t0
-tauC = 4./TsC
-Ta = -8.44
-T0 = 26.5
-time_cool_sim = np.linspace(0,time_cool[-1]-t0,1000)
-temp_cool_sim = (Ta-T0)*(1-np.exp(-tauC*time_cool_sim)) + T0
-plt.plot(time_cool_sim+t0,temp_cool_sim,'r-',label='Fitted Data')
-plt.legend()
+TA = -8.0 ##where the graph settls
+T0 = 26.8 ##this is where the graph starts
 
-time_heat = heat[:,0]
-temp_heat = heat[:,1]
-plt.figure()
-plt.plot(time_heat,temp_heat,'b*',label='Measured Data')
-plt.grid()
-plt.xlabel('Time (sec)')
-plt.ylabel('Temperature (C)')
+### Time_to_settle = 4.0/tau #where time to settle is the value within 2% ish
+time_to_settle = 620.0
+tau = 4.0/time_to_settle
+print(tau)
+t = np.linspace(0,np.max(time_cool),1000)
+T = (TA-T0)*(1-np.exp(-tau*t)) +  T0
 
-t0 = 4.94
-TsH = 350-t0
-tauH = 4./TsH
-Ta = 25.33
-T0 = -1.03
-time_heat_sim = np.linspace(0,time_heat[-1]-t0,1000)
-temp_heat_sim = (Ta-T0)*(1-np.exp(-tauH*time_heat_sim)) + T0
-plt.plot(time_heat_sim+t0,temp_heat_sim,'r-',label='Fitted Data')
-plt.legend()
-
-
+plt.plot(t,T,'r-',label='Numerical Fit')
 
 plt.show()
