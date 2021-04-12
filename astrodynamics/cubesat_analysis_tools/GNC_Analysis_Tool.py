fa01ee7b39924b2dcc2da46da12262130c3c54e3
MJCobar <60676736+MJCobar@users.noreply.github.com>
2021-04-12 13:04:22 -0500

    Update GNC_Analysis_Tool.py
    This is only partially working

--- astrodynamics/cubesat_analysis_tools/GNC_Analysis_Tool.py
+++ astrodynamics/cubesat_analysis_tools/GNC_Analysis_Tool.py
@@ -10,23 +10,28 @@
 
 ###Modules
 import numpy as np
-import wmm2020 as wmm
 import Orbit as O
 
 ###Classes
 class CubeSat():
-    def __init__(input):
-        self.input = input
-        self.Analysis()
-
-    def Analysis(self):
-        self.mag_report = self.Magnetorquer_Analysis()
-        self.rw_report = self.Reaction_Wheel_Analysis()
+    def __init__(self,FS,Ixx,Iyy,Izz,wmax,length,width,height,Mission_Duration,CD,rp,ra):
+        self.Orbit_Analysis()
+        #self.Magnetic_Field_Model_Comparison()
+        self.Disturbance_Torques()
+        self.Reaction_Wheel_Analysis()
+        #self.Magnetorquer_Analysis()
         
     def Orbit_Analysis(self):
         orbit = O.Earth_Orbit(ra,rp)
         orbit.Numerical_Orbit(1000)
         orbit.make_plots()
+        
+    #def Magnetic_Field_Model_Comparison(self):
+        ##do we need this?
+        ## IGRF vs WMM2015
+        ##After doing some research, it seems the WMM is the best option.
+        ##This is because the WMM is produced for the US and UK defense agencies with guaranteed support and updates.
+        ##https://www.ngdc.noaa.gov/geomag/faqgeom.shtml#:~:text=What%20is%20the%20difference%20between%20IGRF%20and%20WMM%20models%3F,-The%20World%20Magnetic&text=The%20WMM%20is%20a%20predictive,for%20the%20years%201900.0%20%2D%202020.0.
 
     def Disturbance_Torques(self):
         ##Compute the disturbance torques based on orbit
@@ -36,7 +41,7 @@ class CubeSat():
         Surface_Area = height*width
         solar_rad_force = Surface_Area*rad_pressure
         solar_rad_torque = solar_rad_force*np.max([height/2.0,width/2.0])
-        ##print('Solar Rad Torque = ',solar_rad_torque)
+        #print('Solar Rad Torque = ',solar_rad_torque)
         ##aerodynamic torques
         
         ##gravity gradient torque
@@ -44,10 +49,10 @@ class CubeSat():
         ##magnetic resonance dipole
         
         ##disturbance torques per axis
-        Disturbance_Torques = aero_torque+solar_rad_torque+grav_torque+mag_res_dipole
+        Disturbance_Torques = solar_rad_torque#+aero_torque+grav_torque+mag_res_dipole
         Disturbance_Torques_per_axis = Disturbance_Torques/np.sqrt(3)
-        d_torques = np.asarray([Disturbance_Torques_per_axis,Disturbance_Torques_per_axis,Disturbance_Torques_per_axis])
-        return d_torques
+        self.d_torques = np.asarray([Disturbance_Torques_per_axis,Disturbance_Torques_per_axis,Disturbance_Torques_per_axis])
+        return self.d_torques
         
     def Reaction_Wheel_Analysis(self):
         ##Given inertia
@@ -59,16 +64,15 @@ class CubeSat():
         I = np.asarray([Ixx,Iyy,Izz])
         w = np.asarray([wx,wy,wz])
         H = I*w
-        Hreq = H*FS
-        #print('Momentum Required (Nms) = ',Hreq)
+        self.Hreq = H*FS
         ##Based on disturbance torques
         ##Can compute number of times during mission to desaturate?
-        Hreq_X = Hreq[0]
-        Hreq_Y = Hreq[1]
-        Hreq_Z = Hreq[2]
+        Hreq_X = self.Hreq[0]
+        Hreq_Y = self.Hreq[1]
+        Hreq_Z = self.Hreq[2]
         
         #X-axis
-        Disturbance_Torques_X = d_torques[0]
+        Disturbance_Torques_X = self.d_torques[0]
         Angular_Accel_X = Disturbance_Torques_X/Ixx #rad/s
         Max_Angular_Velocity_X = Hreq_X/Ixx #rad/s
         Time_to_Sat_sec_X = Max_Angular_Velocity_X/Angular_Accel_X #sec
@@ -77,7 +81,7 @@ class CubeSat():
         Number_of_Desat_Man_Estimation_X = np.ceil(Number_of_Desat_Man_X)
         
         #Y-axis
-        Disturbance_Torques_Y = d_torques[1]
+        Disturbance_Torques_Y = self.d_torques[1]
         Angular_Accel_Y = Disturbance_Torques_Y/Iyy #rad/s
         Max_Angular_Velocity_Y = Hreq_Y/Iyy #rad/s
         Time_to_Sat_sec_Y = Max_Angular_Velocity_Y/Angular_Accel_Y #sec
@@ -86,7 +90,7 @@ class CubeSat():
         Number_of_Desat_Man_Estimation_Y = np.ceil(Number_of_Desat_Man_Y)
         
         #Z-axis
-        Disturbance_Torques_Z = d_torques[2]
+        Disturbance_Torques_Z = self.d_torques[2]
         Angular_Accel_Z = Disturbance_Torques_Z/Izz #rad/s
         Max_Angular_Velocity_Z = Hreq_Z/Izz #rad/s
         Time_to_Sat_sec_Z = Max_Angular_Velocity_Z/Angular_Accel_Z #sec
@@ -94,18 +98,12 @@ class CubeSat():
         Number_of_Desat_Man_Z = Mission_Duration/Time_to_Sat_Months_Z
         Number_of_Desat_Man_Estimation_Z = np.ceil(Number_of_Desat_Man_Z)
         
-        Desat_Mans = np.asarray([Number_of_Desat_Man_Estimation_X,Number_of_Desat_Man_Estimation_Y,Number_of_Desat_Man_Estimation_Z])
-        #print('Number of Desaturation Maneuvers = ',Desat_Mans)
-        return Hreq,Desat_Mans
+        self.Desat_Mans = np.asarray([Number_of_Desat_Man_Estimation_X,Number_of_Desat_Man_Estimation_Y,Number_of_Desat_Man_Estimation_Z])
+        print('Momentum Required (Nms) = ',self.Hreq)
+        print('Number of Desaturation Maneuvers = ',self.Desat_Mans)
+        return self.Hreq,self.Desat_Mans
 
-    def Magnetic_Field_Model_Comparison(self):
-        ##do we need this?
-        ## IGRF vs WMM2015
-        ##After doing some research, it seems the WMM is the best option.
-        ##This is because the WMM is produced for the US and UK defense agencies with guaranteed support and updates.
-        ##https://www.ngdc.noaa.gov/geomag/faqgeom.shtml#:~:text=What%20is%20the%20difference%20between%20IGRF%20and%20WMM%20models%3F,-The%20World%20Magnetic&text=The%20WMM%20is%20a%20predictive,for%20the%20years%201900.0%20%2D%202020.0.
-
-    def Magnetorquer_Analysis(self):
+    #def Magnetorquer_Analysis(self):
         ###Given inertia and orbit compute mag effectivness over 
         ###1 orbit
 
@@ -119,12 +117,9 @@ class CubeSat():
 
         ##This is the range where the magTs are effective
 
-        #return output
-        return effectiveness
+        #return self.effectiveness
 
-####Example script
-
-## Example Inputs
+##Inputs
 example_inputs = np.loadtxt('ABEX_Data_File.txt')
 
 FS = example_inputs[0] #factor of safety
@@ -142,12 +137,9 @@ height = example_inputs[7]/100.0
 Mission_Duration = example_inputs[8] #months
 
 CD = example_inputs[9] #Drag
-rp = example_inputs[10]
-ra = example_inputs[11]
 
-## Run the function above
+rp = example_inputs[10] #perigee
+ra = example_inputs[11] #apogee
 
-
-### print the output
-print('Momentum Required (Nms) = ',Hreq)
-print('Number of Desaturation Maneuvers = ',Desat_Mans)
+##Run the function above
+GNC = CubeSat(FS,Ixx,Iyy,Izz,wmax,length,width,height,Mission_Duration,CD,rp,ra)
