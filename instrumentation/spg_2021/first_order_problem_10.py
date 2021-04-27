703a06120aa0fe54e24e1fce538c237bd95d9f6b
Carlos Montalvo <cmontalvo@southalabama.edu>
2021-04-27 13:36:15 -0500

    Fixed all of the Tau business

--- instrumentation/spg_2021/first_order_problem_10.py
+++ instrumentation/spg_2021/first_order_problem_10.py
@@ -11,27 +11,27 @@ plt.plot(time,temp,'b*',label='Experimental Data')
 
 ###Using the 98% value
 tsettle = 2.0
-tau = 4.0/tsettle 
+tau = tsettle/4.0
 #Tsettle (sec) = 4.0 / tau (sec^-1)
 
 ###Using the 68% value
 #t68 = 0.6
-#tau = 1.0/t68
+#tau = t68
 
-time_constant = 1/tau
+time_constant = tau
 print(time_constant)
 
 #This is Brigg's guess and he was right.
 #tau = 1.0/time_constant #vat about dis? er....no.
 ###Analytic Solution
 time_smooth = np.linspace(0,np.max(time),1000)
-temp_analytic = temp[0]*np.exp(-tau*time_smooth)
+temp_analytic = temp[0]*np.exp(-(1/tau)*time_smooth)
 plt.plot(time_smooth,temp_analytic,'r-',label='Analytic Solution')
 
 ##Numerical Solution
 ##First I need a derivatives routine
 def Derivatives(T,t):
-    Tdot = -tau*T
+    Tdot = -(1/tau)*T
     return Tdot
 ##I need ot call the scipy.integration toolbox
 Temperature_scipy = sci.odeint(Derivatives,temp[0],time_smooth)
