bec8b97899daf11650709db6fac4141211cc6f35
Carlos Montalvo <cmontalvo@southalabama.edu>
2021-08-30 12:38:52 -0500

    Fixed the remove colons script

--- io/remove_colons.py
+++ io/remove_colons.py
@@ -7,17 +7,14 @@ from shutil import *
 
 for root,dirs,files in os.walk('.'):
     for src in files:
-        print "Current File = ",src
         if src.find(':') != -1:
             dst = src.replace(":","_")
-        else:
-            dst = src
-        try:
-            copyfile(src,dst)
-            os.remove(src)
-            print "Changing to....",dst
-        except:
-            print "Skipping File"
+            fullsrc = root + '/' + src
+            fulldst = root + '/' + dst
+            print "Current File = ",fullsrc
+            copyfile(fullsrc,fulldst)
+            os.remove(fullsrc)
+            print "Changing to....",fulldst
 
 # Copyright - Carlos Montalvo 2015
 # You may freely distribute this file but please keep my name in here
