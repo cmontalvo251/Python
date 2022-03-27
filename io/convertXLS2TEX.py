bd81a8d2b03f69266c881d6382eaa423cbcc06aa
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-03-27 12:58:26 -0500

    Added copyright to Lisa for XLS2TEX converter

--- io/convertXLS2TEX.py
+++ io/convertXLS2TEX.py
@@ -1,3 +1,18 @@
+"""
+Hi Carlos,
+
+Pandas is amazing!!! Just wanted to share with you the ability to convert an excel table to a .tex file :) Only three lines of  code.
+"""
+
 import pandas as pd
 df = pd.read_excel(r'Example.xlsx')
 df.to_latex('output.tex')
+
+
+"""
+Thanks,
+Lisa Schibelius
+Pronouns she | her
+Graduate Teaching Assistant | PhD Student
+Virginia Tech |  Department of Engineering Education
+"""
