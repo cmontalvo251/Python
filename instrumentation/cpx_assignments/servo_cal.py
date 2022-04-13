264e49cb311afdec720644e4c5e1ff0a0770fb18
Carlos Montalvo <cmontalvo@southalabama.edu>
2022-04-13 13:41:32 -0500

    Edited servo cal code

--- instrumentation/cpx_assignments/servo_cal.py
+++ instrumentation/cpx_assignments/servo_cal.py
@@ -2,29 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ##DATA
-us = np.array([680,750,1200,1800,2000])
+ms = np.array([680,750,1200,1800,2000])/1000.0
 theta = np.array([11,45,92,135,179])
 
 ###USE POLYFIT TO FIT A LINE
-coeff = np.polyfit(theta,us,1) #1 for linear, 2 for quadratic, etc...
+coeff = np.polyfit(theta,ms,1) #1 for linear, 2 for quadratic, etc...
 print(coeff)
 
 ###PLOT TRENDLINE
 thetatrend = np.linspace(np.min(theta),np.max(theta),1000)
-ustrend = np.polyval(coeff,thetatrend)
+mstrend = np.polyval(coeff,thetatrend)
 
 ##COMPUTE OUTPUT OF TRENDLINE AT XVALUES
 Ytilde = np.polyval(coeff,theta)
 
-plt.plot(theta,us,'b*')
-plt.plot(thetatrend,ustrend,'r-')
+plt.plot(theta,ms,'b*')
+plt.plot(thetatrend,mstrend,'r-')
 plt.plot(theta,Ytilde,'g*')
 plt.xlabel('Degrees')
-plt.ylabel('Microseconds')
+plt.ylabel('Milli Seconds')
 plt.grid()
 
 ###COMPUTE RESIDUALS
-res = (us-Ytilde)**2
+res = (ms-Ytilde)**2
 
 plt.figure()
 plt.plot(theta,res,'b*')
@@ -33,7 +33,7 @@ plt.ylabel('Residuals Squared')
 plt.grid()
 
 ###R^2
-R2 = 1 - np.sum(res) / np.sum((us-np.mean(us))**2)
+R2 = 1 - np.sum(res) / np.sum((ms-np.mean(ms))**2)
 print('R2 = ',R2)
 
 
