aa4eb4ebe47d6e1e92db4a9440f571cab6539a0c
Carlos Montalvo PRESTIGE <cmontalvo251@gmail.com>
2025-04-01 21:08:57 -0500

    edited chapter 6 binomial code to work in python3

--- instrumentation/chapter6_binomial.py
+++ instrumentation/chapter6_binomial.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math as M
 import matplotlib.pyplot as plt
 
 def fact(num):
-    return np.math.factorial(num)
+    return M.factorial(int(num))
 
 def ntaker(n,r):
     return fact(n)/(fact(r)*fact(n-r))
@@ -14,12 +15,13 @@ def prob(n,r,p):
 n = 4.
 r = 4.
 p = 0.95
-#print('P(4) = ',prob(n,r,p))
+print('P(4) = ',prob(n,r,p))
 
 ###Problem 32
 n = 180.
 p = 0.95
-rvec = np.arange(0,181,1)
+rvec = np.arange(0,181,1) #for a cool plot
+rvec = np.arange(176,181,1) #For actual problem 
 prob_total=0.0
 prob_total_vec = np.linspace(0,1,len(rvec))
 prob_vec = np.linspace(0,1,len(rvec))
