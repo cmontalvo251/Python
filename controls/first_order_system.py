5cab746c6fa1ae4975786fb0b83b52f325dd8f2d
Carlos Montalvo <cmontalvo@southalabama.edu>
2025-10-14 15:26:53 -0500

    Edited first order system with extra code for position of car

--- controls/first_order_system.py
+++ controls/first_order_system.py
@@ -10,16 +10,26 @@ m = 1000       # kg
 t = np.linspace(0, 200, 1000)
 # Analytic solution from 
 v_analytic = (F0/c) * (1 - np.exp(-c/m * t))
+x_analytic = (F0/c)*t + (F0*m/c**2)*(np.exp(-c/m*t)-1)
 # Numerical integration
-def eoms(v, t, F0, c, m):
-    return (F0 - c*v)/m
-v_numeric = odeint(eoms,0,t,args=(F0, c, m)).flatten()
+def eoms(z, t, F0, c, m):
+    x = z[0]
+    v = z[1]
+    xdot = v
+    xddot = (F0 - c*v)/m
+    return [xdot, xddot]
+numeric = odeint(eoms, [0, 0], t, args=(F0, c, m))
+v_numeric = numeric[:, 1]
+x_numeric = numeric[:, 0]
 # Laplace (transfer function) solution
 num = [c/m]
 den = [1, c/m]
-system = ctl.tf(num, den)
-t_laplace, v_laplace = ctl.step_response(system, T=t)
+v_system = ctl.tf(num, den)
+x_system = ctl.tf(num, [1, c/m, 0]) #notice the extra zero for position
+t_laplace, v_laplace = ctl.step_response(v_system, T=t)
+t_laplace, x_laplace = ctl.step_response(x_system, T=t)
 v_laplace *= F0/c  # scale for step input magnitude
+x_laplace *= F0/c  # scale for step input magnitude
 # Plotting
 plt.figure(figsize=(8,5))
 plt.plot(t, v_analytic, label='Analytic Solution', lw=2)
@@ -29,5 +39,14 @@ plt.xlabel('Time [s]')
 plt.ylabel('Velocity [m/s]')
 plt.title('First Order System Response')
 plt.legend()
+plt.grid(True)
+
+plt.figure(figsize=(8,5))
+plt.plot(t, x_analytic, label='Analytic Solution', lw=2)
+plt.plot(t, x_numeric,label='Numerical Integration', lw=2)
+plt.plot(t_laplace, x_laplace, ':', label='Laplace Solution', lw=2)
+plt.xlabel('Time [s]')
+plt.ylabel('Position [m]')
+plt.title('Second Order System Position Response (ω_n=0)')
 plt.grid(True)
 plt.show()
